84724fe6e23abdf0e0b656e9c1522aafd9dd1ab4
Renzo Barrios <w3renzo@gmail.com>
2021-01-04 19:47:23 -0300

    Fix: Emojis Display

--- Module03/My-Emojis/index.js
+++ Module03/My-Emojis/index.js
@@ -1,7 +1,6 @@
 let inputEmojis = document.querySelector('input');
 const emojiContainer = document.getElementById('emojiContainer');
-let emojiArray;
-
+let emojiArray = Array.from(inputEmojis.value);
 //btn's
 const btnShift = document.getElementById('shift');
 const btnUnshift = document.getElementById('unshift');
@@ -13,21 +12,41 @@ function getEmojis() {
   console.log(emojiArray);
 }
 
-inputEmojis.addEventListener('input', getEmojis);
+//inputEmojis.addEventListener('input', getEmojis);
+
+
+
+
 
 //Display Emojis
 function displayEmojis() {
-  emojiArray.forEach((element) => {
+  let emojis = [];
+  emojiContainer.innerHTML = '';
+  for (let index = 0; index < emojiArray.length; index++) {
+    let emojiSpan = document.createElement('span');
+    emojiSpan.textContent = emojiArray[index];
+    emojis.push(emojiSpan);
+  }
+
+  console.log(emojis);
+  emojis.forEach((element) => {
     emojiContainer.append(element);
   });
 }
 
-//🥺🥳🥺🥺🥺
+//🥺🥳😅🥺🥺✨
 
 ////Remove from Array listeners
 
-btnPop.addEventListener('click', () => emojiArray.pop());
-btnShift.addEventListener('click', () => emojiArray.shift());
+btnPop.addEventListener('click', () => {
+  emojiArray.pop();
+  displayEmojis();
+});
+
+btnShift.addEventListener('click', () => {
+  emojiArray.shift();
+  displayEmojis();
+});
 
 //Add to array Listeners
 btnUnshift.addEventListener('click', () => {
@@ -35,6 +54,7 @@ btnUnshift.addEventListener('click', () => {
   inputEmojis.value = '';
   displayEmojis();
 });
+
 
 btnPush.addEventListener('click', () => {
   emojiArray.push(inputEmojis.value);
