a1f60ca3f088cbff5e9b451af1f3c655e749c8cf
Renzo Barrios <w3renzo@gmail.com>
2021-01-16 11:03:01 -0300

    Search bar completed

--- Module04/Javascript-Challenges/Build-SearchBar/main.js
+++ Module04/Javascript-Challenges/Build-SearchBar/main.js
@@ -4,9 +4,7 @@ const arrOfNames = arrNames();
 
 search.addEventListener('keyup', () => {
   let inputValue = search.value.toLowerCase().split(/[ ,]+/);
-  console.log(names);;
   resetStyles();
-
   searchOfList(inputValue);
 });
 
@@ -27,43 +25,34 @@ function nodeListToArray(params) {
 
 
 function searchOfList(arrInputs) {
+
+
   for (let index = 0; index < arrInputs.length; index++) {
     let nameFound = arrOfNames.find((name) => name == arrInputs[index]);
     console.log(nameFound);
     let targetIndex = arrOfNames.indexOf(nameFound);
     console.log(targetIndex);
     if (nameFound) {
-
-      
-      names.splice(targetIndex, 1);
-
-      names.forEach((item) => {
-        item.style.opacity = 0.3;
-      });
-
-      names[targetIndex].style.backgroundColor = '#a2d0c1';
+     
+      names[targetIndex].classList.add("selected")
 
     }
   }
+
+  /*g.forEach(name=>{
+    name.classList.remove("name")
+    name.style.backgroundColor = "dodgerblue"
+  })*/
+
+
+
+
 }
 
-/*
-
-function singleQueryParam(inputParam) {
-  let nameFound = arrOfNames.find((name) => name == inputParam);
-
-  let targetIndex = arrOfNames.indexOf(nameFound);
-  if (nameFound) {
-    names[targetIndex].style.backgroundColor = 'red';
-  } else {
-    resetStyles();
-  }
-  console.log(nameFound);
-}*/
 
 function resetStyles() {
   names.forEach((li) => {
     li.style.backgroundColor = '';
-   
+   li.classList.remove("non-selected")
   });
 }
