ce94f52eac0807d45e9ca15db21253ccaf16b13b
Renzo Barrios <w3renzo@gmail.com>
2021-01-15 22:38:11 -0300

    Search bar v2

--- Module04/Javascript-Challenges/Build-SearchBar/main.js
+++ Module04/Javascript-Challenges/Build-SearchBar/main.js
@@ -1,10 +1,10 @@
-const names = document.querySelectorAll('.name');
+const names = Array.from(document.querySelectorAll('.name'));
 const search = document.getElementById('searchBar');
 const arrOfNames = arrNames();
 
-search.addEventListener('keyup', (event) => {
+search.addEventListener('keyup', () => {
   let inputValue = search.value.toLowerCase().split(/[ ,]+/);
-
+  console.log(names);;
   resetStyles();
 
   searchOfList(inputValue);
@@ -20,13 +20,29 @@ function arrNames() {
   return arr;
 }
 
+function nodeListToArray(params) {
+    
+}
+
+
+
 function searchOfList(arrInputs) {
   for (let index = 0; index < arrInputs.length; index++) {
     let nameFound = arrOfNames.find((name) => name == arrInputs[index]);
     console.log(nameFound);
     let targetIndex = arrOfNames.indexOf(nameFound);
+    console.log(targetIndex);
     if (nameFound) {
-      names[targetIndex].style.backgroundColor = 'red';
+
+      
+      names.splice(targetIndex, 1);
+
+      names.forEach((item) => {
+        item.style.opacity = 0.3;
+      });
+
+      names[targetIndex].style.backgroundColor = '#a2d0c1';
+
     }
   }
 }
@@ -48,5 +64,6 @@ function singleQueryParam(inputParam) {
 function resetStyles() {
   names.forEach((li) => {
     li.style.backgroundColor = '';
+   
   });
 }
