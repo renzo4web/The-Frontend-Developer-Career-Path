62880ef37bef5cb4a9f06ef1916eb363e9141f42
Renzo Barrios <w3renzo@gmail.com>
2021-01-18 22:12:56 -0300

    Fix snake movement

--- Module04/Snake-Game/app/app.js
+++ Module04/Snake-Game/app/app.js
@@ -1,20 +1,39 @@
 const grid = document.getElementById('grid');
+//DOM
+const btnStart = document.getElementById("start")
+const btnReset  =document.getElementById("reset")
+const score = document.getElementById("score")
+// Global Vars
+let scoreCount = 0;
+let direction = 1
+let widthRow = 10;
+let limistRow = widthRow
 let divsArr = [];
-let snake = [0, 1, 2];
-gridDivs();
-let count = 1;
+let snake = [3,2,1,0];
+let snakeLoop;
+let rndNum;
+btnStart.addEventListener("click",startGame)
+
+btnReset.addEventListener("click",()=>{
+  clearInterval(snakeLoop)
+})
+
+
+
+
 
 window.addEventListener('keydown', (e) => {
   if (e.key === 'ArrowUp') {
     console.log('arriba');
+    direction = -widthRow
   } else if (e.key === 'ArrowRight') {
     console.log('derecha');
-    count += 1;
-    move();
-    //moveRight();
+    direction = 1;
   } else if (e.key === 'ArrowDown') {
-    console.log('abajo');
+    direction = widthRow
+    
   } else if (e.key === 'ArrowLeft') {
+    direction = -1
     console.log('izquierda');
   }
 });
@@ -22,7 +41,7 @@ window.addEventListener('keydown', (e) => {
 //Generate div for grid
 
 function gridDivs() {
-  for (let i = 0; i < 81; i++) {
+  for (let i = 0; i < 100; i++) {
     const div = document.createElement('div');
     div.classList.add('square');
     div.textContent = i;
@@ -31,22 +50,65 @@ function gridDivs() {
   }
 }
 
-snake.forEach((segment) => {
-  divsArr[segment].textContent = snake.indexOf(segment);
-  divsArr[segment].classList.add('snake');
-});
 
 function move() {
-  /*snake.forEach((segment) => {
-    divsArr[segment].classList.remove('snake');
-  });*/
 
-  for (let i = count; snake.length <= 3; i++) {
-    snake.push(i);
-  }
+  const tails =  snake.pop()
+  // remove class from tails
+  divsArr[tails].classList.remove("snake")
+  
+  // add square in the direcction we heading
+  snake.unshift(snake[0] + direction)
 
-  snake.forEach((segment) => {
-    divsArr[segment].textContent = snake.indexOf(segment);
-    divsArr[segment].classList.add('snake');
-  });
+
+  
+  //add style
+  divsArr[snake[0]].classList.add("snake")
+  
+  if(direction >= 10){
+      limistRow += 10
+  }else if(snake[0] + 1 >= limistRow){
+      console.log("caiste afuera")
+      clearInterval(snakeLoop)
+    }
+  console.log(limistRow)
+  console.log(direction)
+  sumPoint()
+
+
 }
+
+function rndSquarePoint() {
+  rndNum = Math.floor(Math.random() * 98) +1
+  divsArr[rndNum].classList.add("point")
+  
+}
+
+
+function sumPoint() {
+  
+
+  if( snake[0] === rndNum ){
+    divsArr[rndNum].classList.remove("point")
+    console.log("PUNTO!!!")
+    scoreCount ++
+    score.textContent = scoreCount
+    rndSquarePoint()
+  }
+  
+}
+
+function drawSnake() {
+  
+  snake.forEach(i => divsArr[i].classList.add("snake"))
+}
+
+function startGame() {
+  gridDivs();
+  snakeLoop = setInterval(move,1000)
+  rndSquarePoint()
+  drawSnake()
+}
+
+
+
